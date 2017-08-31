20dd54efe9b208f89b81f22854e9a7216591eabb
sarojjango <jangosaroj@gmail.com>
2017-08-31 16:55:55 +0545

    Initial commit

---
+++ app/src/main/java/sarojsharma/choice/hisab/MainActivity.java
@@ -0,0 +1,13 @@
+package sarojsharma.choice.hisab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
